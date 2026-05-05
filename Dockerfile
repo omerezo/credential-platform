6824826dcfc0f793f13ce614ba9c1d7c71bc03d8
unknown <omerezo5@gmail.com>
2026-05-05 23:03:27 +0300

    Fix Dockerfile entrypoint and Railway healthcheck

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM vaultwarden/server:latest
 COPY start.sh /start.sh
 RUN chmod +x /start.sh
 
-CMD ["/start.sh"]
+ENTRYPOINT ["/start.sh"]
+CMD []
--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
+#!/bin/sh
 export ROCKET_PORT="${PORT:-80}"
 export ROCKET_ADDRESS="0.0.0.0"
-export DATA_FOLDER="${DATA_FOLDER:-/data}"
-
 exec /vaultwarden
